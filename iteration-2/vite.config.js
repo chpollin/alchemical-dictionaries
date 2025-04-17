c4d3b083d9fa403a3e8305c48609df28700ffbcb
Christopher Pollin <christopher.pollin@uni-graz.at>
2025-04-17 13:29:45 +0200

    Update vite.config.js

--- iteration-2/vite.config.js
+++ iteration-2/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/alchemical-dictionaries/',   // ⬅ repo name between the slashes
+  base: '/alchemical-dictionaries/iteration-2',   // ⬅ repo name between the slashes
 });
